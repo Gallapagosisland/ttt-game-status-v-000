63d9b7a42cc53a6d89be6aa14b5ee8b95367e967
Connor Gallopo <connor.gallopo@me.com>
2017-11-19 23:51:55 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,56 +1,12 @@
- require "pry"
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
-end
-
-# Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
-]
-
- def won?(board)
-  WIN_COMBINATIONS.detect do |win_combination|
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
-
-
-    position_1 = board[win_index_1]
-    position_2 = board[win_index_2]
-    position_3 = board[win_index_3]
-
-
-    position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
-      #win_combination # return the win_combination indexes that won.
-
-  end
-end
-
-def full?(board)
-  if board.include?(" " || nil)
-    false
-  else
-    true
-  end
-end
-
-def draw?(board)
-full?(board) && !won?(board)
-end
-
-def over?(board)
-won?(board) || full?(board)
-end
-
-def winner(board)
-  if winning = won?(board)
-    board[winning.first]
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
+      return win_combination
+    elsif
+      board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
+      return win_combination
+    else
+      return false
+    end
   end
 end
